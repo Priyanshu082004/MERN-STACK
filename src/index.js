97165667eac81c4a2548f9ae5623baf6f6b87810
Priyanshu082004 <priyanshuu202004@gmail.com>
2026-05-14 19:23:01 +0530

    UTIL CODEFILES(RESPOMSE,ERROR)

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.listem(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running on port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+  console.log("MONGO DB CONNECTION FAILED!!!!",err);
+  
+})
 
 
 
