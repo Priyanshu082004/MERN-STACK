ca99eaf99b3b184cc1533b0ce4de87f526a28401
Priyanshu082004 <priyanshuu202004@gmail.com>
2026-05-20 23:53:58 +0530

    DEBUGGING

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+const _dirname = path.dirname(__filename);
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// require ('dotenv').config({path: './env'})
-
-
+// Load environment variables FIRST before any imports
 import dotenv from "dotenv";
 dotenv.config({
     path: './.env'
@@ -50,14 +48,14 @@ connectDB()
 
 // ;(async ()=>{
 //   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     await mongoose.connect(${process.env.MONGODB_URL}/${DB_NAME})
 //     app.on("error",(error)=>{
 //         console.log("ERROR: ", error);
 //         throw error
 //     })
 
 //     app.listen(process.env.PORT, ()=>{
-//         console.log(`App is listening on port": ${process.env.PORT}`);
+//         console.log(App is listening on port": ${process.env.PORT});
         
 //     })
 //   } catch (error) {
