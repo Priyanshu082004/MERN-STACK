9f99021aa2c4ea6c191d05417616e6976fccbca5
Priyanshu082004 <priyanshuu202004@gmail.com>
2026-05-24 21:16:09 +0530

    Refactor user routes to include additional functionalities for password change, account updates, avatar and cover image uploads, and user channel/profile retrieval.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser,
+   loginUser, 
+   logoutUser,
+    refreshAccesToken, 
+    changeCurrentUserPassword, 
+    getCurrentUser, updateAccountDetails, 
+    updateUserAvatar, updateUserCoverImage ,
+     getUserChannelProfile,
+     getWatchHistoryffyiiig    } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser , logoutUser} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { refreshAccesToken } from "../controllers/user.controller.js";
 const router= Router()
 
 
@@ -22,5 +28,12 @@ router.route("/login").post(loginUser)
 // secured routes 
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccesToken)
+router.route("/change-password").post(verifyJWT,changeCurrentUserPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account-details").patch(verifyJWT,updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/update-coverImage").patch (verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/channel/:username").get(getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT,getWatchHistory)
 
 export default router
